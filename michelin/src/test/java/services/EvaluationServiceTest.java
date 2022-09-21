fcd2a421be0fdff5dd96c17c7424fdcbc128556c
Flynet-BDD <77499285+Flynet-BDD@users.noreply.github.com>
2022-09-21 12:00:23 +0200

    Ajoute EvaluationServiceTest

--- michelin/src/test/java/services/EvaluationServiceTest.java
+++ michelin/src/test/java/services/EvaluationServiceTest.java
@@ -1,6 +1,26 @@
 package services;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import polytech.michelin.exception.ValeurIncorrecte;
+import polytech.michelin.repository.EvaluationRepository;
+import polytech.michelin.service.EvaluationService;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class EvaluationServiceTest 
 {
-    
+    @InjectMocks
+    private EvaluationService evaluationService;
+
+    @Mock
+    private EvaluationRepository evaluationRepository;
+
+    @Test()
+    public void shouldThrowValeurIncorrecte()
+    {
+        assertThrows(ValeurIncorrecte.class, () -> this.evaluationService.addEvaluation(99, "Etchebest", "Miam", 3));
+    }
 }
